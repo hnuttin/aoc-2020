bee632ddc00c89cf9447c21911d76e087b2a6694
hansnuttin <hans.nuttin@aca-it.be>
2020-12-01 17:22:20 +0100

    day1 part2

--- src/main/java/com/hnuttin/aoc2020/day1/Day1.java
+++ src/main/java/com/hnuttin/aoc2020/day1/Day1.java
@@ -3,6 +3,7 @@ package com.hnuttin.aoc2020.day1;
 public class Day1 {
 
     public static void main(String ... args) {
-        System.out.println(ExpenseReport.fromInput(Input1.INPUT1).fix());
+        System.out.println("Part 1: " + ExpenseReport.fromInput(Input1.INPUT1).fix(2));
+        System.out.println("Part 2: " + ExpenseReport.fromInput(Input1.INPUT1).fix(3));
     }
 }
--- src/main/java/com/hnuttin/aoc2020/day1/ExpenseReport.java
+++ src/main/java/com/hnuttin/aoc2020/day1/ExpenseReport.java
@@ -1,47 +1,67 @@
 package com.hnuttin.aoc2020.day1;
 
-import lombok.Value;
+import static java.util.Collections.singletonList;
+import static java.util.stream.Collectors.toList;
 
 import java.util.List;
 import java.util.stream.Stream;
 
+import lombok.AllArgsConstructor;
+import lombok.Value;
+
 @Value
 class ExpenseReport {
 
-    List<Integer> expenses;
+	List<Integer> expenses;
 
-    static ExpenseReport fromInput(List<Integer> input) {
-        return new ExpenseReport(input);
-    }
+	static ExpenseReport fromInput(List<Integer> input) {
+		return new ExpenseReport(input);
+	}
 
-    int fix() {
-        return expensePermutations()
-                .filter(ExpensePermutation::matches2020)
-                .findFirst()
-                .map(ExpensePermutation::multiply)
-                .orElseThrow(IllegalArgumentException::new);
-    }
+	long fix(int permutationLength) {
+		return expensePermutations(permutationLength)
+				.filter(ExpensePermutation::matches2020)
+				.findFirst()
+				.map(ExpensePermutation::multiply)
+				.orElseThrow(IllegalArgumentException::new);
+	}
 
-    private Stream<ExpensePermutation> expensePermutations() {
-        return expenses.stream().flatMap(this::permutationsForExpense);
-    }
+	private Stream<ExpensePermutation> expensePermutations(int permutationLength) {
+		return expenses.stream()
+				.map(ExpensePermutation::singlePermutation)
+				.flatMap(singlePermutation -> extendPermutation(singlePermutation, permutationLength - 1));
+	}
 
-    private Stream<ExpensePermutation> permutationsForExpense(int expense) {
-        return expenses.stream().map(otherExpense -> new ExpensePermutation(expense, otherExpense));
-    }
+	private Stream<ExpensePermutation> extendPermutation(ExpensePermutation permutation, int permutationLength) {
+		if (permutationLength <= 0) {
+			return Stream.of(permutation);
+		} else {
+			return expenses.stream()
+					.map(permutation::extendPermutation)
+					.flatMap(extendedPermutation -> extendPermutation(extendedPermutation, permutationLength - 1));
+		}
+	}
 
-    @Value
-    private static class ExpensePermutation {
+	@Value
+	@AllArgsConstructor
+	private static class ExpensePermutation {
 
-        int expense1;
-        int expense2;
+		List<Integer> expenses;
 
-        boolean matches2020() {
-            return expense1 + expense2 == 2020;
-        }
+		boolean matches2020() {
+			return expenses.stream().reduce(Integer::sum).orElseThrow(IllegalArgumentException::new) == 2020;
+		}
 
-        int multiply() {
-            return expense1 * expense2;
-        }
-    }
+		long multiply() {
+			return expenses.stream().reduce(Math::multiplyExact).orElseThrow(IllegalArgumentException::new);
+		}
+
+		ExpensePermutation extendPermutation(int expense) {
+			return new ExpensePermutation(Stream.concat(expenses.stream(), Stream.of(expense)).collect(toList()));
+		}
+
+		static ExpensePermutation singlePermutation(int expense) {
+			return new ExpensePermutation(singletonList(expense));
+		}
+	}
 }
